899ed12e39f8f9f9d962913c618da6854a4cc486
Jakob Woittennek <jakob@woittennek.net>
2026-06-03 12:11:12 +0200

    add function to create article html with img references

--- viewer.php
+++ viewer.php
@@ -9,4 +9,27 @@ function getArticleList(PDO $db) {
     }
     return $entries;
 }
+function getArticle(PDO $db, $headline) {
+    if ($db->query("SELECT DATABASE()")->fetchColumn() != "bergundsteigen") {
+        $db->exec("USE bergundsteigen");
+    }
+    $entry = $db->prepare("SELECT * FROM articles WHERE Headline = ?");
+    $entry->execute([$headline]);
+    $entry = $entry->fetch(PDO::FETCH_ASSOC);
+    if ($entry) {
+        $imgPath = $entry["Date"] . "_" . str_replace(" ", "_", $entry["Headline"]);
+        $imgUrl = urlencode($imgPath);
+        $entry["Tags"] = explode(",", $entry["Tags"]);
+        $imgs = scanDir($imgPath);
+        foreach ($imgs as $img) {
+            if ($img == "." || $img == "..") { // unix current and parent dir
+                continue;
+            }
+            $imgId = intval(str_replace("image-", "", pathinfo($img, PATHINFO_FILENAME)))+1;
+            $ref = "image-".$imgId."-src";
+            $entry["Content"] = str_replace($ref, $imgUrl."/".$img, $entry["Content"]);
+        }
+        return $entry;
+    }
+}
 ?>
