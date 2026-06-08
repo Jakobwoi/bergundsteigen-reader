f1e78cea2407678e76c8e813302fefacb3d80e18
Jakob Woittennek <jakob@woittennek.net>
2026-06-08 18:51:33 +0200

    add endpoint for getting articles

---
+++ article.php
@@ -0,0 +1,18 @@
+<?php
+require("viewer.php");
+require("config.php");
+if (isset($_GET["offline"])) {
+    $offline = boolval($_GET["offline"]);
+} else {
+    $offline = false;
+} 
+if (isset($_GET["hash"])) {
+    $article = getArticle($db, hash: $_GET["hash"], offline: $offline);
+    echo json_encode($article);
+} elseif (isset($_GET["id"])) {
+    $article = getArticle($db, id: $_GET["id"], offline: $offline);
+} else {
+    die("No article specified");
+}
+echo $article["Content"];
+?>
